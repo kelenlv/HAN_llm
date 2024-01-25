028e7c0294b62598758833b44e2686b45a73770c
lvkexin <Kelen_Lv@sjtu.edu.cn>
2024-01-25 17:30:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -28,13 +28,13 @@ def on_btn_click():
 
 @st.cache_resource
 def load_model():
-    config = AutoConfig.from_pretrained("kelenlv/HAN_llm", trust_remote_code=True)
-    model = AutoModelForCausalLM.from_config(config, trust_remote_code=True)
-    # model = (
-    #     AutoModelForCausalLM.from_pretrained("kelenlv/HAN_llm", trust_remote_code=True)
-    #     .to(torch.bfloat16)
-    #     .cuda()
-    # )
+    # config = AutoConfig.from_pretrained("kelenlv/HAN_llm", trust_remote_code=True)
+    # model = AutoModelForCausalLM.from_config(config, trust_remote_code=True)
+    model = (
+        AutoModelForCausalLM.from_pretrained("kelenlv/HAN_llm", trust_remote_code=True)
+        .to(torch.bfloat16)
+        .cuda()
+    )
     tokenizer = AutoTokenizer.from_pretrained("kelenlv/HAN_llm", trust_remote_code=True)
     return model, tokenizer
 
